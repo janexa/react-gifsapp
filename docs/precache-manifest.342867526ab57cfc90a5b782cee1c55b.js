beff2efec3f968f5a02bdb9549bae2f8f6e39204
Janexa <jnxacc7@gmail.com>
2020-10-08 17:55:19 -0500

    Commit carpetas

---
+++ docs/precache-manifest.342867526ab57cfc90a5b782cee1c55b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fcd254dee42be34063ff402365805d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c029d55251eaa497dc5",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c029d55251eaa497dc5",
+    "url": "/static/js/main.6846255b.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
